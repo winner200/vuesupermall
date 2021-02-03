8f212e9983596f6ecfbf5ac97be23df193d149b4
winner200 <winner443@163.com>
2021-02-03 21:23:02 +0800

    2

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,26 +2,30 @@
   <div>
     <goods-detail-nav-bar/>
     <goods-detail-swiper :swiper-images="topImages"/>
+    <detail-base-info :goods-info="goods"/>
   </div>
 </template>
 
 <script>
 
 import GoodsDetailNavBar from "./childComps/GoodsDetailNavBar";
-import GoodsDetailSwiper from "@/views/detail/childComps/GoodsDetailSwiper";
+import GoodsDetailSwiper from "./childComps/GoodsDetailSwiper";
+import DetailBaseInfo from './childComps/DetailBaseInfo'
 
-import {getDetail} from 'network/detail'
+import {getDetail, Goods} from 'network/detail'
   export default {
     name: "Detail",
     data() {
       return {
         iid: null,
-        topImages:[]
+        topImages:[],
+        goods: {}
       }
     },
     components: {
       GoodsDetailSwiper,
-      GoodsDetailNavBar
+      GoodsDetailNavBar,
+      DetailBaseInfo
     },
     created() {
       console.log('-----',this.$route.params.iid)
@@ -31,13 +35,20 @@ import {getDetail} from 'network/detail'
     },
     methods: {
       _getDetail() {
+        // 1.根据iid请求详情数据
         getDetail(this.iid).then(res => {
           console.log('商品详情数据请求成功', res)
-          this.topImages = res.result.itemInfo.topImages
+          const data = res.result
+          // 1.获取顶部的图片轮播图数据
+          this.topImages = data.itemInfo.topImages
+
+          // 2.获取商品信息
+          this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
         }).catch(error=> {
           console.log('商品详情数据请求失败', error)
         })
-      }
+      },
     }
   }
 </script>
---
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="border">
+    <div class="title">
+      <p>{{goodsInfo.title}}</p>
+    </div>
+    <div class="price">
+      <span class="price-1">{{goodsInfo.newPrice}}</span>
+      <span class="price-2">{{goodsInfo.oldPrice}}</span>
+      <img src="" alt="">
+    </div>
+    <div class="statistics">
+      <span v-for="item in goodsInfo.columns">{{item}}</span>
+    </div>
+    <div class="after-sale">
+      <div v-for="index in goodsInfo.services.length-1">
+        <img :src="goodsInfo.services[index-1].icon" alt="">
+        <span>{{goodsInfo.services[index-1].name}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailBaseInfo",
+  props: {
+    goodsInfo: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .price, .statistics, .after-sale, .title{
+   padding: 0px 10px 0px 10px;
+   font-size: var(--font-size);
+ }
+ .title {
+   margin: 10px 0px 5px 0px;
+   line-height: 1.7;
+ }
+ .price {
+   height: 50px;
+
+   display: flex;
+   align-items: center;
+  }
+ .price-1 {
+   font-size: 25px;
+   color: var(--color-text);
+ }
+ .price-2 {
+   width: 20px;
+   color: #cdcdcd;
+
+   margin: 5px 0px 0px 10px;
+ }
+ .price-2 :after{
+   background-color: red;
+ }
+  .statistics {
+    height: 30px;
+    color: #aeadad;
+
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #cdcdcd;
+
+  }
+  .after-sale {
+    height: 50px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    border-bottom: 5px solid #ededed;
+  }
+ .after-sale div {
+   display: flex;
+   align-items: center;
+ }
+  .after-sale div img {
+    width: 15px;
+    height: 15px;
+    margin-right: 5px;
+  }
+</style>
